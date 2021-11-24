16e96f580df0a27c5fd15fb6016f7e6801dab7f2
aakash404 <aakashkumbhare@gmail.com>
2021-11-24 12:04:02 +0530

    Updated code on timestamp:   24-11-2021 - 12:04:02.06

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "My Site",
+        title: "Documentation",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
@@ -88,7 +88,7 @@ const config = {
               },
               {
                 label: "Twitter",
-                href: "https://twitter.com/docusaurus",
+                href: "https://twitter.com/aakash_kumbhare",
               },
             ],
           },
